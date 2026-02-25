986ae407581484ceb9f783f33ac05db3d776555a
Aqib Digital Hub <aqibdigitalhub@gmail.com>
2026-02-25 12:21:13 +0500

    Add files via upload

---
+++ day2-practical-program.py
@@ -0,0 +1,7 @@
+name = input("Enter your name")
+age = int(input("Enter your age:"))
+monthly-goal= float(input("Enter your monthly earning goal:"))          
+
+print("Hello" , name)
+print("Next year you will be" , age + 1)
+monthly_goal = float(input("Enter your monthly earning goal:"))
