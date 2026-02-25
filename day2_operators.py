6850d00e176a9d73ed8a2afff4886ac11eea6798
Aqib Digital Hub <aqibdigitalhub@gmail.com>
2026-02-25 12:43:41 +0500

    Add files via upload

---
+++ day2_operators.py
@@ -0,0 +1,10 @@
+age = int(input("Enter your age:"))
+monthly_goal = float(input("Enter your monthly earning goal:"))
+
+next_year_age = age + 1
+half_goal = monthly_goal / 2
+double_goal = monthly_goal * 2
+
+print("Next year you will be:",next_year_age)
+print("Half of your earning goal:",half_goal )
+print("Double of your earning goal:",double_goal)
