f1c19d3f51138d13e63f1c9493e1474d7177564f
Aqib Digital Hub <aqibdigitalhub@gmail.com>
2026-02-26 09:50:12 +0500

    Add files via upload

---
+++ day2_final_practical.py
@@ -0,0 +1,13 @@
+# User input
+name = input("Enter your name:")
+age = int(input("Enter your age:"))
+city = input("Enter your city:")
+
+#Mini string operations
+greeting = "Hello" + name + "!" 
+future_age = age + 1
+
+# Output
+print(greeting)
+print("Next year you will be",future_age, "years old")
+print("you live in", city)
