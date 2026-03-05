8ffee36ffe96d6a9f4c09fcf81400c12291b36ca
Aqib Digital Hub <aqibdigitalhub@gmail.com>
2026-03-05 11:14:43 +0500

    Add files via upload

---
+++ day3_age_advisor.py
@@ -0,0 +1,9 @@
+age = int(input("Enter your age: "))
+if age < 18:
+    print("Focus on your studies and learning skills.")
+elif age >= 18 and age < 25:
+    print("Focus on building skills and career.")
+elif age >= 25 and age < 60:
+    print("Focus on earning, career groth, and investment.")
+else:
+    print("Focus on health and peaceful life.")    
