9f5ad6a671ad6f671d3276615efe96b9d17a4a79
Aqib Digital Hub <aqibdigitalhub@gmail.com>
2026-04-14 13:44:25 +0500

    Add files via upload

---
+++ day5_function.py
@@ -0,0 +1,15 @@
+def multiply(a, b):
+    return a * b 
+result1 = multiply(5, 2)
+result2 = multiply(10, 3)
+print(result1)
+print(result2)
+def squre(number):
+    return number * number
+print(squre(4))
+print(squre(6))
+def calculator(a, b):
+    print("Addiction =",a + b)
+    print("Subtraction =", a - b)
+    print("Multiplication =", a * b)
+    calculator(10, 5)
